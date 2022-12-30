fa311b63f7627fcf5ad5a7e7ba7d4ba8cab5711a
tuhin201765 <tuhin201765@gmail.com>
2022-12-30 11:12:43 +0600

    More instruction file added

--- ass21.py
+++
@@ -1,6 +0,0 @@
-import searchconsole
-
-account = searchconsole.authenticate(client_config='client_secret.json')
-webproperty = account['https://localhost:8080/']
-report = webproperty.query.range('today', days=7).dimension('query').get()
-print(report.rows)
